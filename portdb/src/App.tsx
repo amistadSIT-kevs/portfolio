f0a2be0f7cdc2e14271e6c89e4e027bebbc33c65
amistadSIT-kevs <20255677@s.ubaguio.edu>
2026-05-08 14:15:05 +0800

    app.tsx

--- portdb/src/App.tsx
+++ portdb/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// Changed BrowserRouter to HashRouter
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navigation from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
@@ -15,7 +16,11 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
 
 function App() {
   return (
-    <Router basename="/portfolio">
+    /* 
+       When using HashRouter, you generally do not need the 'basename' prop 
+       because the routing starts after the '#' symbol.
+    */
+    <Router>
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -28,6 +33,8 @@ function App() {
             <Dashboard />
           </ProtectedRoute>
         } />
+        {/* Redirect any unknown hash routes to Home */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
